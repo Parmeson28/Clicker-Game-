9a203d14af541f792431b66a5ebe0d255b078871
Fewpi7owo <felpz9898@gmail.com>
2025-09-22 21:23:15 -0300

    oii

--- core/src/main/java/com/javaProjects/game/Main.java
+++ core/src/main/java/com/javaProjects/game/Main.java
@@ -1,11 +1,13 @@
 package com.javaProjects.game;
 
+import java.util.Vector;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -14,22 +16,41 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 public class Main extends ApplicationAdapter {
 
     private SpriteBatch batch;
-    private Texture image;
+    private Texture blahajTexture;
 
     private Object BlahajObj;
 
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+    Sprite blahajSprite;
+
+    Vector2 touchPos;
+
 
     @Override
-    public void create() {
+    public void create() {    // create is executed immediatly when the game is run
 
-        batch = new SpriteBatch();
-        image = new Texture("Blahaj.png");
-        
-        Button button = new Button();
+        blahajTexture = new Texture("blahaj2.png");
 
-        Gdx.graphics.setWindowedMode(1280, 720);
+       // Gdx.graphics.setWindowedMode(1280, 720); da pra mudar no Clickergames/lwjgl3/src/main/java/com/javaProjects/game/lwjgl3/Lwjgl3Launcher.java
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(1280, 720);
+        blahajSprite = new Sprite(blahajTexture);
+        blahajSprite.setSize(510, 300);
+
+
+        touchPos = new Vector2();
+    }
+
+    @Override
+    public void resize(int width, int height){
+
+        viewport.update(width, height, true);
 
     }
+
 
     @Override
     public void render() {
@@ -41,6 +62,12 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input(){
+
+        if (Gdx.input.isTouched()){
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(touchPos);
+            blahajSprite.setCenter(touchPos.x, touchPos.y);
+        }
         
     }
 
@@ -50,17 +77,21 @@ public class Main extends ApplicationAdapter {
 
     private void draw(){
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
 
-        batch.begin();
+        spriteBatch.begin();
+       
+        blahajSprite.draw(spriteBatch);
 
-        batch.draw(image, 40, 20);
         
-        batch.end();
+        spriteBatch.end();
+      
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        blahajTexture.dispose();
     }
 }
